9df15acb29827b90972e973650fdae4bac06ad39
SaiSankeerthreddy <saisankeerthobulam@gmail.com>
2020-09-24 20:54:16 +0530

    calendar fixed

--- src/components/MarketingExecutive/Appointments/Appointments.jsx
+++ src/components/MarketingExecutive/Appointments/Appointments.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import { Agenda, CalendarList } from 'react-native-calendars';
+import { StyleSheet, View } from 'react-native';
+import { CalendarList } from 'react-native-calendars';
 import { getAppointmentList } from '../../../networkcalls/apiCalls';
 import { theme } from '../../../theme/theme';
 import { getValue } from '../../../utils/asyncStorage';
@@ -15,6 +15,9 @@ const styles = StyleSheet.create({
 export const Appointments = ({ navigation }) => {
 
   const [showSpinner, setShowSpinner] = useState(false)
+  const [appointmentData, setAppointmentData] = useState([])
+  const [datesMarked, setDatesMarked] = useState(null)
+
 
   useEffect(() => {
     console.log('did load')
@@ -32,6 +35,22 @@ export const Appointments = ({ navigation }) => {
       .then((apiResponse) => {
         setShowSpinner(false)
         console.log('apiResponse', apiResponse)
+        if (apiResponse.data.status == "success") {
+          console.log('apiResponse', apiResponse.data.response.appointments)
+          const data = apiResponse.data.response.appointments
+          setAppointmentData(data)
+
+          let dataPush = [];
+          data.forEach(element => {
+            const convertDate = element.appointmentStartDate.slice(0, 10)
+            dataPush.push(convertDate)
+            return dataPush;
+          });
+
+          let obj = dataPush.reduce((c, v) => Object.assign(c, { [v]: { marked: true, dotColor: theme.colors.LIGHT_GRAY } }), {});
+          setDatesMarked(obj)
+        }
+
       })
       .catch((error) => {
         setShowSpinner(false)
@@ -64,73 +83,33 @@ export const Appointments = ({ navigation }) => {
 
   const renderCalendar = () => {
     return (
-      <Agenda
-        // The list of items that have to be displayed in agenda. If you want to render item as empty date
-        // the value of date key has to be an empty array []. If there exists no value for date key it is
-        // considered that the date in question is not yet loaded
-        items={{
-          '2020-10-22': [{ name: 'item 1 - any js object' }],
-          '2020-10-23': [{ name: 'item 2 - any js object', height: 80 }],
-          '2020-10-24': [],
-          '2020-10-25': [{ name: 'item 3 - any js object' }, { name: 'any js object' }]
-        }}
-        // Callback that gets called when items for a certain month should be loaded (month became visible)
-        loadItemsForMonth={(month) => { console.log('trigger items loading') }}
-        // Callback that fires when the calendar is opened or closed
-        onCalendarToggled={(calendarOpened) => { console.log(calendarOpened) }}
-        // Callback that gets called on day press
-        onDayPress={(day) => { console.log('day pressed') }}
-        // Callback that gets called when day changes while scrolling agenda list
-        onDayChange={(day) => { console.log('day changed') }}
-        // Initially selected day
-        selected={new Date()}
-        // Minimum date that can be selected, dates before minDate will be grayed out. Default = undefined
-        minDate={new Date()}
-        // Maximum date that can be selected, dates after maxDate will be grayed out. Default = undefined
-        maxDate={new Date(2022, 0, 16)}
+      <CalendarList
+        // Callback which gets executed when visible months change in scroll view. Default = undefined
+        onVisibleMonthsChange={(months) => { console.log('now these months are visible', months); }}
         // Max amount of months allowed to scroll to the past. Default = 50
         pastScrollRange={50}
         // Max amount of months allowed to scroll to the future. Default = 50
         futureScrollRange={50}
-        // Specify how each item should be rendered in agenda
-        renderItem={(item, firstItemInDay) => { return (<View />); }}
-        // Specify how each date should be rendered. day can be undefined if the item is not first in that day.
-        renderDay={(day, item) => { return (<View />); }}
-        // Specify how empty date content with no items should be rendered
-        renderEmptyDate={() => { return (<View />); }}
-        // Specify how agenda knob should look like
-        renderKnob={() => { return (<View />); }}
-        // Specify what should be rendered instead of ActivityIndicator
-        renderEmptyData={() => { return (<View />); }}
-        // Specify your item comparison function for increased performance
-        rowHasChanged={(r1, r2) => { return r1.text !== r2.text }}
-        // Hide knob button. Default = false
-        hideKnob={false}
-        // By default, agenda dates are marked if they have at least one item, but you can override this if needed
-        markedDates={{
-          '2020-10-16': { selected: true, marked: true },
-          '2020-10-17': { marked: true },
-          '2020-10-18': { disabled: true }
-        }}
-        // If disabledByDefault={true} dates flagged as not disabled will be enabled. Default = false
-        disabledByDefault={false}
-        // If provided, a standard RefreshControl will be added for "Pull to Refresh" functionality. Make sure to also set the refreshing prop correctly.
-        onRefresh={() => console.log('refreshing...')}
-        // Set this true while waiting for new data from a refresh
-        refreshing={false}
-        // Add a custom RefreshControl component, used to provide pull-to-refresh functionality for the ScrollView.
-        refreshControl={null}
+        // Enable or disable scrolling of calendar list
         scrollEnabled={true}
-        // Agenda theme
+        // Enable or disable vertical scroll indicator. Default = false
+        showScrollIndicator={true}
+        onDayPress={(day) => { console.log('day pressed', day) }}
+        markedDates={
+          datesMarked
+        }
         theme={{
-          // ...calendarTheme,
-          agendaDayTextColor: 'yellow',
-          agendaDayNumColor: 'green',
-          agendaTodayColor: 'red',
-          agendaKnobColor: 'blue'
+          'stylesheet.day.basic': {
+            today: {
+              borderRadius: 16,
+              backgroundColor: theme.colors.VIVID_BLUE
+            },
+            todayText: {
+              color: theme.colors.WHITE,
+              fontWeight: '500',
+            },
+          },
         }}
-        // Agenda container style
-        style={{}}
       />
     )
   }
